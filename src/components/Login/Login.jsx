d0a8ce4f2a30fcd3e7fe36235f1ab1096ac3cbce
itss0urav <sourav2021official@gmail.com>
2023-02-02 18:48:13 +0530

    signup and login form design

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
         <input type="text" placeholder='Enter your username'/>
         <input type="text" placeholder='Enter your password'/>
         <button>Login</button>
-        <span className={styles.signup}>New to Anywhere ?<Link to="/signUp" className='link'> Sign up</Link></span>
+        <span className={styles.signup}>New to Anywhere ?<Link to="/signUp" className='link'> Sign Up</Link></span>
       </form>
     </div>
   )
