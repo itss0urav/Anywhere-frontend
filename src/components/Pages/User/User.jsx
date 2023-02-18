48fef79c85ca52a3b4a35546102439df842dfa13
itss0urav <sourav2021official@gmail.com>
2023-02-18 20:17:41 +0530

    latest

--- src/components/Pages/User/User.jsx
+++ src/components/Pages/User/User.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "./User.module.css";
 import Vote from "../../Vote/Vote";
+import { useContext } from "react";
+import { UserContext } from './../../../context/UserContext';
 
 const UserProfile = ({
-  name,
   totalPosts,
   voteCount,
   timeOfAccountCreation,
 }) => {
+  const {user} = useContext(UserContext)
   return (
     <div className={styles.wrappper}>
       <div className={styles.innerWrappper}>
         <h3 className="user-profile-title">User Profile</h3>
-        <p className="user-profile-name">Name: {name}</p>
+        <p className="user-profile-name">Name: {user}</p>
         <p className="user-profile-posts">Total Posts: {totalPosts}</p>
         <p className="user-profile-votes">Votes: {voteCount}</p>
         <p className="user-profile-creation-time">
