4c0a9517c682b7e7afe51ad0e25614f4a299ac73
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-02-20 19:37:59 +0530

    Get post completed

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,35 +2,24 @@ import React, { useState } from "react";
 import styles from "./Post.module.css";
 import Vote from "../Vote/Vote";
 import { Link } from "react-router-dom";
-const Post = (postData) => {
+const Post = ({imgUrl,title, description}) => {
   return (
     <div className={styles.postWrap}>
-      {/* <div className={styles.arrowWrap}>
-        <i className="fa-solid fa-arrow-up"></i>
-        <p>100</p>
-        <i className="fa-solid fa-arrow-down"></i>
-      </div> */}
       <Vote />
 
       <div className={styles.wrap}>
         <div className={styles.userNAme}>
           <p>Posted by Author</p>
         </div>
-        <p className={styles.title}>IKKA FANS (KARACHI UNITED)</p>
+        <p className={styles.title}>{title}</p>
         <div className={styles.imageWrap}>
           <img
-            src="https://media.discordapp.net/attachments/979241917852303370/1054756965273845820/Picsart_22-12-20_19-16-40-819.jpg?width=1191&height=670"
+            src={imgUrl}
             alt="computer"
           />
         </div>
         <p className={styles.desc}>
-          "Ikka fans" refers to fans of Indian film actor Mammootty. He is a
-          prominent film actor in the Indian film industry, particularly in the
-          Malayalam film industry. He has acted in over 400 films and is
-          considered one of the greatest actors in Indian cinema. His fans
-          admire his acting skills, versatility, and charismatic screen
-          presence. They are passionate about his movies and follow his work
-          closely.
+         {description}
         </p>
         <div className={styles.feedBacks}>
           <Link to="/Comment">
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,20 +1,34 @@
 import styles from "./Posts.module.css";
 import Post from "../Post/Post";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../Pages/Sidebar/Sidebar";
+import {useQuery} from "react-query"
 import Categories from "../Categories/Categories";
+import { PostServices } from "../../services/postServices";
 const Posts = () => {
   const { user } = useContext(UserContext);
+  const [posts, setPosts] = useState([])
+  const postServices = new PostServices()
   const navigate = useNavigate();
-
-  console.log(user);
+  useQuery({
+    queryFn:postServices.getPosts,
+    queryKey:["posts"],
+    onSuccess:(data) => setPosts(data)
+  })
 
   return (
     <div className={styles.wrap}>
       <div className={styles.innerWrap}>
-        <Post />
+        <div>
+        {
+          posts.map((post) => (
+            <Post imgUrl={post.imageUrl} title={post.title} description={post.description}/>
+          ))
+        }
+
+        </div>
         <Sidebar />
         <Categories />
       </div>
