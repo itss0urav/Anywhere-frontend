7271833ac1e432e36c0d4e8307dcad13456f2c6b
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-05-06 11:02:57 +0530

    Notification added

--- src/components/Pages/Requirements/Requirements.jsx
+++ src/components/Pages/Requirements/Requirements.jsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useContext, useRef, useState } from "react";
 import styles from "./Requirements.module.css";
+import { UserContext } from "../../../context/UserContext";
+import axios from "axios";
+import { storage } from "../../../firebase/config";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 const VerificationPage = () => {
+  const emailRef = useRef()
+  const companyRef = useRef()
+  const websiteRef = useRef()
+  const fullNameRef = useRef()
+  const [governmentId, setGovernmentId] = useState()
+  const {userId} = useContext(UserContext)
+  
+  const handleVerificationRequest = async function(e){
+    e.preventDefault()
+    let fileUrl
+    if(governmentId){
+      const fileName = Date.now() + governmentId.name;
+      const storageRef = ref(storage, fileName);
+      await uploadBytes(storageRef, image).then((snapshot) => {
+        console.log(snapshot);
+      });
+     fileUrl = await getDownloadURL(ref(storage, fileName));
+    }
+const requestObj = {
+  fullName:fullNameRef.current.value,
+  company:companyRef.current.value,
+  website:websiteRef.current.value,
+  email:emailRef.current.value,
+  userId,
+  governmentId:fileUrl
+}
+
+const response = await axios.post("http://localhost:5000/verification",requestObj)
+response && console.log(response)
+  }
+
   return (
     <div className={styles.requirementsWrap}>
       <h1>Verification Requirements</h1>
@@ -22,45 +57,45 @@ const VerificationPage = () => {
         get started.
       </p>
       <p>Please fill out the form below and submit the required documents:</p>
-      <form>
+      <form onSubmit={handleVerificationRequest}>
 
         <label>
           Full Name:
-          <input type="text" name="name" required />
+          <input type="text" ref={fullNameRef} name="name" required />
         </label>
         <br />
 
         <label>
           Email Address:
-          <input type="email" name="email" required />
+          <input type="email" ref={emailRef} name="email" required />
         </label>
         <br />
 
         <label>
           Company:
-          <input type="text" name="company" required />
+          <input type="text" ref={companyRef} name="company" required />
         </label>
         <br />
 
         <label>
           Website:
-          <input type="text" name="website" required />
+          <input type="text" ref={websiteRef} name="website" required />
         </label>
         <br />
 
         <label>
           Government-Issued ID:
-          <input type="file" name="id" accept="image/*,.pdf" required />
+          <input type="file" name="id" onChange={(e) => setGovernmentId(e.target.files[0])} accept="image/*,.pdf" required />
         </label>
         <br />
 
-        <label>
+        {/* <label>
           Proof of Address:
           <input type="file" name="address" accept="image/*,.pdf" required />
-        </label>
+        </label> */}
         <br />
 
-        <button type="submit">Submit</button>
+        <button type="submit" >Submit</button>
       </form>
       
     </div>
