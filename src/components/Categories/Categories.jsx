73c0df566afe866e113681383f7dcba91a2dae97
itss0urav <sourav2021official@gmail.com>
2023-05-16 20:31:29 +0530

    some minor fix

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -5,47 +5,47 @@ import axios from "axios";
 import { callApi } from "../../services/callApi";
 import { UserContext } from "../../context/UserContext";
 
-const Categories = () =>{
-const { setPosts } = useContext(UserContext)
-  const [categories, setCategories] = useState([])
-useQuery({
-  queryKey:["Categories"],
-  queryFn:async () => {
-    return await callApi({
-      relativePath:"/category",
-      method:"get"
-    })
-  },
-  onSuccess:({data}) => setCategories(data)
-})
+const Categories = () => {
+  const { setPosts } = useContext(UserContext);
+  const [categories, setCategories] = useState([]);
 
-const filterPosts = async function(categoryName){
+  useQuery({
+    queryKey: ["Categories"],
+    queryFn: async () => {
+      return await callApi({
+        relativePath: "/category",
+        method: "get",
+      });
+    },
+    onSuccess: ({ data }) => setCategories(data),
+  });
 
-const response = await callApi({
-  relativePath:`/post?category=${categoryName}&&isPrefix=${true}`,
-  method:"get"
-})
-response && setPosts(response?.data)
-}
-
-
-
+  const filterPosts = async function (categoryName) {
+    const response = await callApi({
+      relativePath: `/post?category=${categoryName}&&isPrefix=${true}`,
+      method: "get",
+    });
+    response && setPosts(response?.data);
+  };
 
   return (
     <div className={styles.categorylist}>
       <h2 className={styles.categorylisttitle}>Categories</h2>
-      <div style={{display:"flex", flexDirection:"column", gap:10  }}>
-
-     {
-      categories.map((category) => (
-        <p style={{cursor:"pointer"}} onClick={() => filterPosts(category?.name)}>{category?.name}</p>
-      ))
-     }
+      <div className={styles.categorylistcontainer}>
+        <div className={styles.scrollablecontainer}>
+          {categories.map((category) => (
+            <p
+              key={category.name}
+              style={{ cursor: "pointer" }}
+              onClick={() => filterPosts(category?.name)}
+            >
+              {category?.name}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
-  )
-} 
-
-
+  );
+};
 
 export default Categories;
